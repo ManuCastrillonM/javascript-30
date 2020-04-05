150842a725277c4a061893c0c7a8bee46f39978b
Manu <manu.cm43@gmail.com>
2020-04-05 16:25:55 -0500

    add day 2

--- 02-css-js-clock/main.js
+++ 02-css-js-clock/main.js
@@ -13,7 +13,7 @@ function setDate() {
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 60) * 360) + 90;
+  const hoursDegrees = ((hours / 12) * 360) + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
